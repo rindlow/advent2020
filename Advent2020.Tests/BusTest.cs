c9f47cd99f01629785d8fa8553f0293717d99367
Henrik Rindlöw <henrik@rindlow.se>
2020-12-13 09:48:46 +0100

    Implement Bus

--- Advent2020.Tests/BusTest.cs
+++ Advent2020.Tests/BusTest.cs
@@ -7,17 +7,19 @@ namespace Advent2020
         public void TestNextBus()
         {
              Bus bus = new Bus();
-             bus.ParseSchedule("7,13,x,x,59,x,31,19");
+             bus.ParseSchedule(”7,13,x,x,59,x,31,19”);
              Assert.Equal(59, bus.NextBus(939));
         }
+        [Fact]
         public void TestMinutesToBus()
         {
             Assert.Equal(5, Bus.MinutesToBus(59, 939));
         }
+        [Fact]
         public void TestNextBusTimesWaitFromFile()
         {
             Bus bus = new Bus();
-            Assert.Equal(295, bus.NextBusTimesWaitFromFile("input/day13.txt"));
+            Assert.Equal(295, bus.NextBusTimesWaitFromFile(”input/day13.txt”));
         }
     }
 }
---
+++ Advent2020/Bus.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+namespace Advent2020
+{
+    public class Bus
+    {
+        private int ArrivalTime;
+        private List<int> Buses;
+        
+        public Bus()
+        {
+            ArrivalTime = 0;
+            Buses = new List<int>(); 
+        }
+        public void ParseSchedule(string schedule)
+        {
+            foreach (string busid in schedule.Split(’,’)
+            {
+                if (busid == ’x’)
+                {
+                    continue; 
+                }
+                Buses.Add(int.Parse(busid));
+            }
+        }
+        public static int MinutesToBus(int bus, int arrival)
+        {
+            return bus - (arrival % bus);
+        }
+        public int NextBus(int arrival)
+        {
+            int minWait = 10000;
+            int nextBus = 0;
+            foreach (int bus in Buses)
+            {
+                int wait = MinutesToBus(bus, arrival);
+                if (wait < minWait)
+                {
+                    minWait = wait;
+                    nextBus = bus;
+                }
+            }
+            return nextBus;
+        }
+        public void ReadFile(string filename)
+        {
+            List<string> file = FileReader.ReadFileOfStrings(filename);
+            ArrivalTime = int.Parse(file[0]);
+            ParseSchedule(file[1]);
+        }
+        public int NextBusTimesWaitFromFile(string filename)
+        {
+            int bus;
+            int wait;
+            ReadFile(filename);
+            bus = NextBus(ArrivalTime);
+            wait = MinutesToBus(bus, ArrivalTime);
+            return bus * wait;
+        }
+    }
+}
