b97c102947bd0a12b5861191edfb58bdc7f634ab
Henrik Rindlöw <henrik@rindlow.se>
2020-12-13 09:22:04 +0100

    Tests for day 13.

---
+++ Advent2020.Tests/BusTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+namespace Advent2020
+{
+    public class BusTest
+    {
+        [Fact]
+        public void TestNextBus()
+        {
+             Bus bus = new Bus();
+             bus.ParseSchedule("7,13,x,x,59,x,31,19");
+             Assert.Equal(59, bus.NextBus(939));
+        }
+        public void TestMinutesToBus()
+        {
+            Assert.Equal(5, Bus.MinutesToBus(59, 939));
+        }
+        public void TestNextBusTimesWaitFromFile()
+        {
+            Bus bus = new Bus();
+            Assert.Equal(295, bus.NextBusTimesWaitFromFile("input/day13.txt"));
+        }
+    }
+}
