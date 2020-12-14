d85f8737b553d0ef80aeab86d306d6678541ce8f
Henrik Rindlöw <henrik@rindlow.se>
2020-12-14 15:30:22 +0100

    Day13 OK

--- Advent2020/Bus.cs
+++ Advent2020/Bus.cs
@@ -6,23 +6,22 @@ namespace Advent2020
     public class Bus
     {
         private int ArrivalTime;
-        private List<int> Buses;
+        private List<long?> Buses;
         
         public Bus()
         {
             ArrivalTime = 0;
-            Buses = new List<int>(); 
+            Buses = new List<long?>(); 
         }
         public void ParseSchedule(string schedule)
         {
-            foreach (string busid in schedule.Split(’,’))
+            foreach (string busid in schedule.Split(','))
             {
-                if (busid == ”x”)
+                if (busid == "x")
                 {
                     Buses.Add(null);
                     continue; 
                 }
-                Console.WriteLine($”found bus ’{busid}’”);
                 Buses.Add(int.Parse(busid));
             }
         }
@@ -34,7 +33,7 @@ namespace Advent2020
         {
             int minWait = 10000;
             int nextBus = 0;
-            foreach (int bus in Buses.Where(bus != null))
+            foreach (int bus in Buses.Where(bus => bus != null))
             {
                 int wait = MinutesToBus(bus, arrival);
                 if (wait < minWait)
@@ -48,7 +47,6 @@ namespace Advent2020
         public void ReadFile(string filename)
         {
             List<string> file = FileReader.ReadFileOfStrings(filename);
-            Console.WriteLine($”parsing ArrivalTime ’{file[0]}’”);
             ArrivalTime = int.Parse(file[0]);
             ParseSchedule(file[1]);
         }
@@ -61,26 +59,61 @@ namespace Advent2020
             wait = MinutesToBus(bus, ArrivalTime);
             return bus * wait;
         }
+        public static long ExtendedGCD(long a, long b, out long x, out long y)
+        {
+            long oldR = a;
+            long r = b;
+            long oldS = 1;
+            long s = 0;
+            long oldT = 0;
+            long t = 1;
+            while (r != 0)
+            {
+                long quotient = oldR / r;
+                long tmp = r;
+                r = oldR - quotient * r;
+                oldR = tmp;
+                tmp = s;
+                s = oldS - quotient * s;
+                oldS = tmp;
+                tmp = t;
+                t = oldT - quotient * t;
+                oldT = tmp;
+            }
+            x = oldS;
+            y = oldT;
+            return oldR;
+        }
         public long StartTimestamp()
         {
-            int x = 0;
-            long timestamp;
-            bool found = false;
-            while (!found)
+            long N = Buses.Where(bus => bus != null).Aggregate(1L, (a, b) => (long)a * (long)b, a => a);
+            long x = 0;
+            int i = 1;
+            foreach (int bus in Buses.Where(bus => bus != null).OrderByDescending(x => x))
             {
-                found = true;
-                timestamp = x * Buses[0]
-                for (int i = 1; i < Buses.Count; i++)
-                {
-                    if (Buses[i] != null && (timestamp + i) % Buses[i] != 0)
-                    {
-                        found = false;
-                        break;
-                    }
-                }
-                x++;
+                int n1 = bus;
+                long N1 = N / n1;
+                long m1;
+                long M1;
+                int a1 = n1 - Buses.IndexOf(n1);
+                ExtendedGCD(n1, N1, out m1, out M1);
+                x += a1 * M1 * N1;
+                i++;
             }
-            return timestamp;
+            while (x < 0)
+            {
+                x += N;
+            }
+            while (x > N)
+            {
+                x -= N;
+            }
+            return x;
+        }
+        public long StartTimestampFromFile(string filename)
+        {
+            ReadFile(filename);
+            return StartTimestamp();
         }
     }
 }
