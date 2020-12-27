f4217ded74ec8d527f64a25e31a08f562b0cd4a2
Henrik Rindlöw <henrik@rindlow.se>
2020-12-27 14:43:13 +0100

    Optimize for speed (1h36m -> 16s)

--- Advent2020/CrabCups.cs
+++ Advent2020/CrabCups.cs
@@ -24,34 +24,103 @@ namespace Advent2020.CrabCups
         {
             Elements = elements;
         }
-
-        public List<T> Remove(int index, int count)
+        public T After(T item)
         {
+            return Elements[(Elements.IndexOf(item) + 1) % Elements.Count];
+        }
+        public List<T> RemoveAfter(T item, int count)
+        {
+            // Console.WriteLine($"RemoveAfter({item}, {count}");
             List<T> res = new List<T>();
+            int index = Elements.IndexOf(item) + 1;
+            // Console.WriteLine($".IndexOf({item}) = {Elements.IndexOf(item)}, index = {index}");
             while (res.Count < count)
             {
                 index %= Elements.Count;
                 res.Add(Elements[index]);
-                Elements.RemoveAt(index);   
+                Elements.RemoveAt(index);
             }
             return res;
         }
-        public void Insert(int index, List<T> elements)
+        public void InsertAfter(T item, List<T> elements)
         {
-            Elements.InsertRange(index, elements);
+            Elements.InsertRange((Elements.IndexOf(item) + 1) % Elements.Count, elements);
         }
-        public int IndexOf(T item)
+    }
+    class RingElement<T>
+    {
+        public T Value { get; }
+        public RingElement<T> Next { get; set; }
+        public RingElement(T value)
         {
-            return Elements.IndexOf(item);
+            Value = value;
+            Next = null;
         }
-        public override string ToString()
+    }
+    class FastRing<T>
+    {
+        Dictionary<T, RingElement<T>> Index;
+        public int Count
         {
-            return string.Join("  ", Elements);
+            get
+            {
+                return Index.Count;
+            }
+        }
+        public FastRing(IEnumerable<T> input)
+        {
+            Index = new Dictionary<T, RingElement<T>>();
+            RingElement<T> firstElement = null;
+            RingElement<T> lastElement = null;
+            foreach (T item in input)
+            {
+                RingElement<T> element = new RingElement<T>(item);
+                Index[item] = element;
+                if (firstElement == null)
+                {
+                    firstElement = element;
+                }
+                if (lastElement != null)
+                {
+                    lastElement.Next = element;
+                }
+                lastElement = element;
+            }
+            lastElement.Next = firstElement;
+        }
+        public T After(T item)
+        {
+            return Index[item].Next.Value;
+        }
+        public List<T> RemoveAfter(T item, int count)
+        {
+            List<T> res = new List<T>();
+            RingElement<T> cur = Index[item];
+            while (res.Count < count)
+            {
+                T value = cur.Next.Value;
+                res.Add(value);
+                cur.Next = cur.Next.Next;
+                Index.Remove(value);
+            }
+            return res;
+        }
+        public void InsertAfter(T item, List<T> elements)
+        {
+            RingElement<T> cur = Index[item];
+            foreach (T value in elements)
+            {
+                RingElement<T> element = new RingElement<T>(value);
+                Index[value] = element;
+                element.Next = cur.Next;
+                cur.Next = element;
+                cur = element;
+            }
         }
     }
     public class Cups
     {
-        Ring<int> Circle;
+        FastRing<int> Circle;
         int Current;
         int Lowest;
         int Highest;
@@ -63,19 +132,19 @@ namespace Advent2020.CrabCups
             {
                 labels.Add(i);
             }
-            Console.WriteLine($"#labels = {labels.Count}");
-            Circle = new Ring<int>(labels);
+            // Console.WriteLine($"#labels = {labels.Count}");
+            Circle = new FastRing<int>(labels);
             Lowest = labels.Min();
             Highest = labels.Max();
-            Console.WriteLine($"Lowest = {Lowest} Highest = {Highest}");
-            Current = 0;
+            // Console.WriteLine($"Lowest = {Lowest} Highest = {Highest}");
+            Current = labels[0];
         }
         void Move()
         {
             // Console.WriteLine($"cups: {Circle}");
-            List<int> removed = Circle.Remove(Circle.IndexOf(Current) + 1, 3);
+            List<int> removed = Circle.RemoveAfter(Current, 3);
             // Console.WriteLine($"pick up: {string.Join(", ", removed)}");
-            int destination = Circle[Circle.IndexOf(Current)] - 1;
+            int destination = Current - 1;
             if (destination < Lowest)
             {
                 // Console.WriteLine($"destination under {Lowest}, wrapping to {Highest}");
@@ -92,34 +161,34 @@ namespace Advent2020.CrabCups
                 }
             }
             // Console.WriteLine($"destination: {destination}");
-            Circle.Insert(Circle.IndexOf(destination) + 1, removed);
-            Current = Circle[Circle.IndexOf(Current) + 1];
+            Circle.InsertAfter(destination, removed);
+            Current = Circle.After(Current);
             // Console.WriteLine();
         }
         public string LabelAfterMoves(int moves)
         {
-            Current = Circle[0];
             for (int i = 0; i < moves; i++)
             {
                 Move();
             }
-            return string.Join("", Circle.Remove(Circle.IndexOf(1) + 1, Circle.Count - 1).Select(i => i.ToString()));
+            return string.Join("", Circle.RemoveAfter(1, Circle.Count - 1).Select(i => i.ToString()));
         }
-        public long ProductOfFirstCups()
+        public long ProductOfFirstCups(int moves)
         {
-            Circle.Insert(Circle.Count, Enumerable.Range(Circle.Count + 1, 1000000 - Circle.Count).ToList());
-            Current = Circle[0];
-            for (int i = 0; i < 10000000; i++)
+            // Circle.Insert(Circle.Count, Enumerable.Range(Circle.Count + 1, 1000000 - Circle.Count).ToList());
+            // Current = Circle[0];
+            for (int i = 0; i < moves; i++)
             {
-                if (i % 1000 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                // if (i % 1000 == 0)
+                // {
+                //     Console.WriteLine(i);
+                // }
                 Move();
             }
-            long first = Circle[Circle.IndexOf(1) + 1];
-            long second = Circle[Circle.IndexOf(1) + 2];
-            Console.WriteLine($"first = {first} second = {second}");
+            List<int> firstPair = Circle.RemoveAfter(1, 2);
+            long first = firstPair[0];
+            long second = firstPair[1];
+            // Console.WriteLine($"first = {first} second = {second}");
             return first * second;
         }
     }
